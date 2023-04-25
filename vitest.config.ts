14586a193fb4c0db5880bd060d2b11b873baa38c
Kelly <long.redhair.tmge.106@gmail.com>
2023-04-25 17:12:26 +0900

    first commit

---
+++ vitest.config.ts
@@ -0,0 +1,19 @@
+import { fileURLToPath } from 'node:url'
+import { mergeConfig } from 'vite'
+import { configDefaults, defineConfig } from 'vitest/config'
+import viteConfig from './vite.config'
+
+export default mergeConfig(
+  viteConfig,
+  defineConfig({
+    test: {
+      environment: 'jsdom',
+      exclude: [...configDefaults.exclude, 'e2e/*'],
+      root: fileURLToPath(new URL('./', import.meta.url)),
+      coverage: {
+        reporter: ['text', 'json', 'html'],
+        reportsDirectory: './__tests__/unit/coverage'  // レポート出力するパスを設定
+        }
+    }
+  })
+)
